76a349446c6f8403819028a05d4ac120d1fa9dd3
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-10-26 19:13:31 +0700

    add json parse to get user from search

--- app/src/main/java/com/example/githubapp/MainActivity.kt
+++ app/src/main/java/com/example/githubapp/MainActivity.kt
@@ -4,36 +4,107 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
+    private val USER_TOKEN = "token 76804862f46181ed1aaa82cf10ca8c691193b982"
     private lateinit var githubUsers: RecyclerView
-    private var  list: ArrayList<Github> = arrayListOf()
+
+    //ini buat nampilin
+    private val listUserAdapter = ListUserAdapter().apply {
+        setOnItemClickCallback(object : ListUserAdapter.OnItemClickCallback {
+            override fun onItemClick(data: Github) {
+                navigateToDetail(data)
+            }
+        })
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        bindSearchView()
+
         githubUsers = findViewById(R.id.rv_github)
         githubUsers.setHasFixedSize(true)
 
-        list.addAll(UserData.listData)
         showRecycleList()
+    }
+
+    private fun bindSearchView() {
+        search_view.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                searchUsers(query)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+    }
+
+    private fun searchUsers(username: String?) {
+        val client = AsyncHttpClient()
+        progressBar.visibility = View.VISIBLE
+
+        val url = "https://api.github.com/search/users?q=$username"
+        client.addHeader("Authorization", USER_TOKEN)
+        client.addHeader("User-Agent", "request")
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray
+            ) {
+                progressBar.visibility = View.INVISIBLE
+                val listUser = ArrayList<Github>()
+                val result = String(responseBody)
+                try {
+                    val responseObject = JSONObject(result)
+                    val items = responseObject.getJSONArray("items")
+
+                    for (i in 0 until items.length()) {
+                        val item = items.getJSONObject(i)
+                        val username = item.getString("login")
+                        val avatar = item.getString("avatar_url")
+                        val user = Github()
+
+                        user.username = username
+                        user.avatar = avatar
+                        listUser.add(user)
+                    }
+                    //point nomor 3 : nampilin di adapter
+                    listUserAdapter.listUser = listUser
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>?,
+                responseBody: ByteArray?,
+                error: Throwable?
+            ) {
+                progressBar.visibility = View.INVISIBLE
+            }
+        })
     }
 
     private fun showRecycleList() {
         githubUsers.layoutManager = LinearLayoutManager(this)
-        val listUserAdapter = ListUserAdapter().apply {
-            listUser = list
-            setOnItemClickCallback(object : ListUserAdapter.OnItemClickCallback {
-                override fun onItemClick(data: Github) {
-                    navigateToDetail(data)
-                }
-            })
-        }
         githubUsers.adapter = listUserAdapter
     }
 
