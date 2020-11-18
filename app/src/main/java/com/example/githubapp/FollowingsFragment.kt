9cf70d171c8742b97dc6e94a283da6fc0aa5b71a
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-11-18 15:51:07 +0700

    fix fragment following to get data

--- app/src/main/java/com/example/githubapp/FollowingsFragment.kt
+++ app/src/main/java/com/example/githubapp/FollowingsFragment.kt
@@ -6,10 +6,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.loopj.android.http.AsyncHttpClient
 import com.loopj.android.http.AsyncHttpResponseHandler
 import cz.msebera.android.httpclient.Header
-import org.json.JSONObject
+import kotlinx.android.synthetic.main.fragment_followings.*
+import org.json.JSONArray
 import java.lang.Exception
 
 class FollowingsFragment : Fragment() {
@@ -17,7 +20,7 @@ class FollowingsFragment : Fragment() {
     companion object {
         private val ARG_USERNAME = "username"
         const val USER_TOKEN = "76804862f46181ed1aaa82cf10ca8c691193b982"
-        private val TAG = FollowingsFragment::class.java.simpleName
+        private val TAG = DetailActivity::class.java.simpleName
 
         fun newInstance(username: String): FollowingsFragment {
             val fragment = FollowingsFragment()
@@ -26,8 +29,10 @@ class FollowingsFragment : Fragment() {
             fragment.arguments = bundle
             return fragment
         }
-
     }
+
+    private lateinit var githubFollowings: RecyclerView
+    private val listFollowingsAdapter = FollowingsAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,15 +43,22 @@ class FollowingsFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        TODO("akses API dari getfollowings method")
-        val username = arguments?.getString(FollowingsFragment.ARG_USERNAME)
+
         super.onViewCreated(view, savedInstanceState)
+        githubFollowings = rv_followings
+        githubFollowings.setHasFixedSize(true)
+
+        val username = arguments?.getString(ARG_USERNAME)
         getfollowings(username)
+
+        showRecycleList()
     }
 
     private fun getfollowings(username: String?) {
         val client = AsyncHttpClient()
-        val url = "https://api.github.com/search/users?q=$username"
+        progressBarrr.visibility = View.VISIBLE
+
+        val url = "https://api.github.com/users/$username/following"
         client.addHeader("Authorization", FollowingsFragment.USER_TOKEN)
         client.addHeader("User-Agent", "request")
         client.get(url, object : AsyncHttpResponseHandler() {
@@ -55,15 +67,26 @@ class FollowingsFragment : Fragment() {
                 headers: Array<out Header>?,
                 responseBody: ByteArray
             ) {
-//                progressBar.visibility = View.INVISIBLE
+                progressBarrr.visibility = View.INVISIBLE
+                val listFollowings = ArrayList<Github>()
                 val result = String(responseBody)
                 Log.d(FollowingsFragment.TAG, result)
                 try {
-                    val item = JSONObject(result)
+                    val items = JSONArray(result)
 
-                    val followingsList = item.getString("followings_url")
-                    val user = Github()
-                    user.followings = followingsList
+                    for (i in 0 until items.length()) {
+                        val item = items.getJSONObject(i)
+                        val avatar = item.getString("avatar_url")
+                        val username = item.getString("login")
+                        val user = Github()
+
+                        user.username = username
+                        user.avatar = avatar
+                        listFollowings.add(user)
+                    }
+
+                    listFollowingsAdapter.listFollowings = listFollowings
+
                     Log.d(FollowingsFragment.TAG, "onSuccess: Selesai....")
 
                 } catch (e: Exception) {
@@ -78,9 +101,13 @@ class FollowingsFragment : Fragment() {
                 responseBody: ByteArray,
                 error: Throwable?
             ) {
-//                progressBar.visibility = View.INVISIBLE
                 Log.d(FollowingsFragment.TAG, "onFailure: Gagal .....")
             }
         })
+    }
+
+    private fun showRecycleList() {
+        githubFollowings.layoutManager = LinearLayoutManager(activity)
+        githubFollowings.adapter = listFollowingsAdapter
     }
 }
