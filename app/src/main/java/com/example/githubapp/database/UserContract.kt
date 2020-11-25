50fb5f5fe75ad3ac496e24afef5fc2ec4c32413d
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-11-25 19:50:51 +0700

    add user database contract

---
+++ app/src/main/java/com/example/githubapp/database/UserContract.kt
@@ -0,0 +1,19 @@
+package com.example.githubapp.database
+
+import android.provider.BaseColumns
+
+class UserContract {
+    class UserColumns: BaseColumns {
+        companion object {
+            val TABLE_NAME = "favorite_user"
+            val COLUMN_NAME_USERNAME = "username"
+            val COLUMN_NAME_AVATAR_URL = "avatar_url"
+            val COLUMN_NAME_COMPANY = "company"
+            val COLUMN_NAME_LOCATION = "location"
+            val COLUMN_NAME_FOLLOWINGS = "following"
+            val COLUMN_NAME_FOLLOWERS = "followers"
+            val COLUMN_NAME_REPOSITORY = "public_repos"
+            val COLUMN_NAME_FULL_NAME = "name"
+        }
+    }
+}
