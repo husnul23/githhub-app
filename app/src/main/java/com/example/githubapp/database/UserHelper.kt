adfe1df034ea483632e093b974450b895a90a7d3
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-11-27 15:15:43 +0700

    add database helper

---
+++ app/src/main/java/com/example/githubapp/database/UserHelper.kt
@@ -0,0 +1,61 @@
+package com.example.githubapp.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import com.example.githubapp.database.UserContract.UserColumns.Companion.COLUMN_NAME_FULL_NAME
+import com.example.githubapp.database.UserContract.UserColumns.Companion.COLUMN_NAME_USERNAME
+import com.example.githubapp.database.UserContract.UserColumns.Companion.TABLE_NAME
+import java.sql.SQLException
+
+class UserHelper(context: Context) {
+
+    companion object {
+        private var DATABASE_TABLE = TABLE_NAME
+        private lateinit var dataBaseHelper: DatabaseHelper
+
+        private var INSTANCE: UserHelper? = null
+        fun getInstance(context: Context): UserHelper =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: UserHelper(context)
+            }
+
+        private lateinit var database: SQLiteDatabase
+
+    }
+
+    init {
+        dataBaseHelper = DatabaseHelper(context)
+    }
+
+    @Throws(SQLException::class)
+    fun open() {
+        database = dataBaseHelper.writableDatabase
+    }
+
+    fun close() {
+        dataBaseHelper.close()
+        if (database.isOpen)
+            database.close()
+    }
+
+    fun queryAll(): Cursor {
+        return database.query(
+            DATABASE_TABLE,
+            null,
+            null,
+            null,
+            null,
+            null,
+            "$COLUMN_NAME_FULL_NAME ASC")
+    }
+
+    fun insert(values: ContentValues?): Long {
+        return database.insert(DATABASE_TABLE, null, values)
+    }
+
+    fun deleteById(username: String): Int {
+        return database.delete(DATABASE_TABLE, "$COLUMN_NAME_USERNAME = '$username'", null)
+    }
+}
