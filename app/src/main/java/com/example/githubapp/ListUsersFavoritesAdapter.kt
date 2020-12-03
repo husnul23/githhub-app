b6172adad5de70f951654011e89cf9acda38268b
fadhilah.acsl@gmail.com <36095304+husnul23@users.noreply.github.com>
2020-12-03 10:46:18 +0700

    work on progrres

--- app/src/main/java/com/example/githubapp/ListUsersFavoritesAdapter.kt
+++ app/src/main/java/com/example/githubapp/ListUsersFavoritesAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ExpandableListView
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -21,6 +22,12 @@ class ListUsersFavoritesAdapter : RecyclerView.Adapter<ListUsersFavoritesAdapter
         notifyDataSetChanged()
     }
 
+    fun setOnItemClickCallback(
+        onItemClickCallback: OnItemClickCallback
+    ) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_row_favorites, parent, false)
         return ListViewHolder(view)
@@ -32,7 +39,7 @@ class ListUsersFavoritesAdapter : RecyclerView.Adapter<ListUsersFavoritesAdapter
 
     override fun onBindViewHolder(holder: ListUsersFavoritesAdapter.ListViewHolder, position: Int) {
         val github = listUser[position]
-        holder.bind(github)
+        holder.bind(github, onItemClickCallback)
     }
 
     interface OnItemClickCallback {
@@ -42,12 +49,12 @@ class ListUsersFavoritesAdapter : RecyclerView.Adapter<ListUsersFavoritesAdapter
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private var githubUsername: TextView = itemView.findViewById(R.id.tvUserFavorite)
         private var githubAvatar: ImageView = itemView.findViewById(R.id.userFavAvatar)
-//        private var githubBtn: Button = itemView.findViewById(R.id.action_favorites)
+        private var deleteFavorite: Button = itemView.findViewById(R.id.deleteFav)
 
-        fun bind(github: Github) {
-//            githubBtn.setOnClickListener {
-//                clickListener.onItemClick(github)
-//            }
+        fun bind(github: Github, clickListener: OnItemClickCallback) {
+            deleteFavorite.setOnClickListener {
+                clickListener.onItemClick(github)
+            }
 
             Glide.with(itemView.context)
                 .load(github.avatar)
--- app/src/main/java/com/example/githubapp/UsersFavoritesActivity.kt
+++ app/src/main/java/com/example/githubapp/UsersFavoritesActivity.kt
@@ -1,29 +1,52 @@
 package com.example.githubapp
 
+import android.content.Intent
+import android.database.ContentObserver
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.githubapp.database.MappingHelper
+import com.example.githubapp.database.UserContract.UserColumns.Companion.CONTENT_URI
 import com.example.githubapp.database.UserHelper
 import kotlinx.android.synthetic.main.toolbar.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
+import kotlin.math.log
 
 class UsersFavoritesActivity : AppCompatActivity() {
 
-    private val listUsersFavoritesAdapter = ListUsersFavoritesAdapter()
+    private val listUsersFavoritesAdapter = ListUsersFavoritesAdapter().apply {
+        setOnItemClickCallback(object : ListUsersFavoritesAdapter.OnItemClickCallback {
+            override fun onItemClick(data: Github) {
+//                showAlertDialog()
+            }
+        })
+    }
+
     private lateinit var userHelper: UserHelper
+    private var position: Int = 0
+    private var github: Github? = null
 
     companion object{
+        private const val EXTRA_STATE = "EXTRA_STATE"
         const val EXTRA_USER = "extra_user"
-        const val EXTRA_FAVORITE = "extra_favorite"
+        const val RESULT_DELETE = 301
         const val EXTRA_POSITION = "extra_position"
+        const val ALERT_DIALOG_CLOSE = 10
+        const val ALERT_DIALOG_DELETE = 20
         private lateinit var githubUsers: RecyclerView
+        private val TAG = UsersFavoritesActivity::class.java.simpleName
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,14 +65,29 @@ class UsersFavoritesActivity : AppCompatActivity() {
         userHelper = UserHelper.getInstance(applicationContext)
         userHelper.open()
 
+        github = intent.getParcelableExtra(EXTRA_USER)
         initRecycleList()
-        loadUsersAsync()
+
+        val handlerThread = HandlerThread("DataObserver")
+        handlerThread.start()
+        val handler = Handler(handlerThread.looper)
+        val myObserver = object : ContentObserver(handler) {
+            override fun onChange(self: Boolean) {
+                loadUsersAsync()
+                Log.d(UsersFavoritesActivity.TAG, "Working oncreate")
+            }
+        }
+
+        contentResolver.registerContentObserver(CONTENT_URI, true, myObserver)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
                 onBackPressed()
+            }
+            R.id.deleteFav -> {
+                showAlertDialog(ALERT_DIALOG_DELETE)
             }
         }
         return super.onOptionsItemSelected(item)
@@ -59,18 +97,55 @@ class UsersFavoritesActivity : AppCompatActivity() {
         GlobalScope.launch(Dispatchers.Main) {
 //            progressbar.visibility = View.VISIBLE
             val deferredUsers = async(Dispatchers.IO) {
-                val cursor = userHelper.queryAll()
+                val cursor = contentResolver?.query(CONTENT_URI, null, null, null, null)
+                Log.d(UsersFavoritesActivity.TAG, "Working load")
                 MappingHelper.mapCursorToArrayList(cursor)
+
             }
 //            progressbar.visibility = View.INVISIBLE
             val users = deferredUsers.await()
             if (users.size > 0) {
                 listUsersFavoritesAdapter.listUser = users
+                Log.d(UsersFavoritesActivity.TAG, "Working load bawah banget")
             } else {
                 listUsersFavoritesAdapter.listUser = ArrayList()
                 Toast.makeText(this@UsersFavoritesActivity, "Data not Found", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    private fun showAlertDialog(type: Int) {
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+        val dialogTitle: String
+        val dialogMessage: String
+
+        val uriWithUsername = Uri.parse(CONTENT_URI.toString() + "/" + github?.username)
+
+        if (isDialogClose) {
+            dialogTitle = "Batal"
+            dialogMessage = "Apakah anda ingin membatalkan perubahan pada favorite?"
+        } else {
+            dialogMessage = "Apakah anda yakin ingin menghapus item ini?"
+            dialogTitle = "Hapus favorite"
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setTitle(dialogTitle)
+        alertDialogBuilder
+            .setMessage(dialogMessage)
+            .setCancelable(false)
+            .setPositiveButton("Ya") { dialog, id ->
+                if (isDialogClose) {
+                    finish()
+                } else {
+                    contentResolver.delete(uriWithUsername, null, null)
+                    Toast.makeText(this, "berhasil dihapus", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+            .setNegativeButton("Tidak") { dialog, id -> dialog.cancel() }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
     private fun initRecycleList() {
